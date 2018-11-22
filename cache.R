33c44c8cf7077eefd0f48e87b9314d33d355f2f0
BeliMove <45251621+BeliMove@users.noreply.github.com>
2018-11-22 16:07:01 +1100

    Add files via upload

---
+++ cache.R
@@ -0,0 +1,25 @@
+makecacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinversematrix <- function(ineverseMatrix) m <<- ineverseMatrix
+  getinversematrix <- function() m
+  list(set = set, get = get,
+       setinversematrix = setinversematrix,
+       getinversematrix = getinversematrix)
+}
+
+cacheSolve <- function(x, ...) {
+  m <- x$getinversematrix()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- Inverse(data, ...)
+  x$setinversematrix(m)
+  m
+}
